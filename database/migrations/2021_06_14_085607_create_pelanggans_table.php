1c71b35ad5de830cade1453cc49896f398180758
= <=>
2021-06-21 16:41:58 +0700

    Membuat Tabel Pelanggan

---
+++ database/migrations/2021_06_14_085607_create_pelanggans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePelanggansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggan', function (Blueprint $table) {
+            $table->id('id_pelanggan');
+            $table->string('nama');
+            $table->text('foto');
+            $table->enum('jk', ['Laki-laki', 'Perempuan']);
+            $table->string('alamat');
+            $table->date('tgl_lahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelanggan');
+    }
+}
